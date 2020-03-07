bbc6159deec58bc4fbff330c38d2b9690940b62d
Teamcity <teamcity@catalyst.github>
2020-03-07 16:13:13 +0000

    last changes

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.2.4d',
+    VERSION: '20.2.4.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
